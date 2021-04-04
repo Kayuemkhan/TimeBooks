97afff2e36ae8c541d5dfc942fd614263c04ca8c
Kayuemkhan <abdulkayuem007@gmail.com>
2021-04-04 17:41:03 +0600

    todo apps with kotlin

--- app/src/main/java/com/example/timebooks/NotesActivity.kt
+++ app/src/main/java/com/example/timebooks/NotesActivity.kt
@@ -2,10 +2,18 @@ package com.example.timebooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NotesActivity : AppCompatActivity() {
+    lateinit var fab_icon:FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes)
+        fab_icon = findViewById(R.id.fab_icon)
+        fab_icon.setOnClickListener(View.OnClickListener {
+            var alertDialog: AlertDialog.Builder = AlertDialog.Builder(this)
+        })
     }
 }
